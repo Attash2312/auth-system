7da9398608575009eb0e946488edc5fe0d868dc3
Attash <attash2312@gmail.com>
2024-05-23 00:04:56 +0500

    .

--- index.js
+++ index.js
@@ -8,21 +8,22 @@ const userController = require('./controllers/userController');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://localhost:27017/auth-system', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/auth-system')
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
+// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
+// Routes
 app.post('/users', userController.createUser);
 app.post('/users/login', userController.loginUser);
 app.get('/users', userController.getUsers);
 app.get('/users/:id', userController.getUserById);
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
